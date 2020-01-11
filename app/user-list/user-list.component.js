bd0dc83ff6fe1e67e5a18693bfffcb9fa541bded
Leila El Ghrib <leila.elghrib@gmail.com>
2020-01-11 19:27:20 +0100

    feat/displayUsers: add API call to /users & /users/count

--- app/user-list/user-list.component.js
+++ app/user-list/user-list.component.js
@@ -2,45 +2,23 @@
 
 angular.module("userList").component("userList", {
   templateUrl: "user-list/user-list.html",
-  controller: function UserListController() {
-    this.count = 6;
-    this.users = [
-      {
-        id: 172,
-        firstName: "Yan",
-        lastName: "dezdhhg",
-        email: "sss@s"
-      },
-      {
-        id: 173,
-        firstName: "jack",
-        lastName: "davis",
-        email: "jack@live.fr"
-      },
-      {
-        id: 223,
-        firstName: "test",
-        lastName: "test",
-        email: "test@test.awd"
-      },
-      {
-        id: 229,
-        firstName: "test",
-        lastName: "test",
-        email: "test@test.com"
-      },
-      {
-        id: 230,
-        firstName: "hello",
-        lastName: "adwdw",
-        email: "awd@adw.awd"
-      },
-      {
-        id: 232,
-        firstName: "jean",
-        lastName: "dor",
-        email: "dor@f.com"
-      }
-    ];
-  }
+  controller: [
+    "$http",
+    function UserListController($http) {
+      var self = this;
+      // self.orderProp = "lastName";
+
+      $http
+        .get("https://trainee-api.pleiads.fr/users")
+        .then(function(response) {
+          self.users = response.data;
+        });
+
+      $http
+        .get("https://trainee-api.pleiads.fr/users/count")
+        .then(function(response) {
+          self.count = response.data.count;
+        });
+    }
+  ]
 });
--- app/user-list/user-list.component.spec.js
+++ app/user-list/user-list.component.spec.js
@@ -2,19 +2,51 @@ describe("userList", function() {
   beforeEach(module("userList"));
 
   describe("UserListController", function() {
-    it("should create a `users` list with 6 users", inject(function(
-      $componentController
-    ) {
-      var ctrl = $componentController("userList");
+    var $httpBackend, ctrl;
 
-      expect(ctrl.users.length).toBe(6);
+    beforeEach(inject(function($componentController, _$httpBackend_) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET("https://trainee-api.pleiads.fr/users").respond([
+        {
+          id: 1,
+          firstName: "Alice",
+          lastName: "Merveille",
+          email: "alice@mail.com"
+        },
+        {
+          id: 2,
+          firstName: "Coco",
+          lastName: "Rico",
+          email: "coco@mail.com"
+        }
+      ]);
+      $httpBackend
+        .expectGET("https://trainee-api.pleiads.fr/users/count")
+        .respond({ count: 2 });
+      ctrl = $componentController("userList");
     }));
 
-    it("should show a total number of users equal to 6", inject(function(
-      $componentController
-    ) {
-      var ctrl = $componentController("userList");
+    it("should fetch 2 users", inject(function() {
+      expect(ctrl.users).toBeUndefined();
+      $httpBackend.flush();
+      expect(ctrl.users).toEqual([
+        {
+          id: 1,
+          firstName: "Alice",
+          lastName: "Merveille",
+          email: "alice@mail.com"
+        },
+        {
+          id: 2,
+          firstName: "Coco",
+          lastName: "Rico",
+          email: "coco@mail.com"
+        }
+      ]);
+    }));
 
+    it("should show a total number of users equal to 2", inject(function() {
+      $httpBackend.flush();
       expect(ctrl.count).toBe(ctrl.users.length);
     }));
   });
